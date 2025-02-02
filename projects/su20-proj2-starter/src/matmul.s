717009212222742b3abfa39b665104a6232cf66a
Zane-Liao <lzq666amn@gmail.com>
2025-02-02 17:42:48 +0800

    Pause ASM

--- projects/su20-proj2-starter/src/matmul.s
+++ projects/su20-proj2-starter/src/matmul.s
@@ -24,48 +24,25 @@
 # =======================================================
 matmul:
 
+# Pause coding ......
+
     # Error checks
+    # Check matrix m0 rows and columns 
     ble a1, zero, exit_92
     ble a2, zero, exit_92
+    # Check matrix m0 rows and columns 
     ble a4, zero, exit_93
     ble a5, zero, exit_93
     bne a2, a4, exit_94
     # Prologue
-    li t0, 0
-    li t1, 0
-    li t2, 0
-    li s0, 0
-
 
 outer_loop_start:
-    blt t0, a1, outer_loop_end
-    j inner_loop_start
 
 inner_loop_start:
-    lw s1, 0(a2)
-    lw s2, 0(a5)
-    lw s3, 0(a0)
-    lw s4, 0(a3)
-    lw s5, 0(a6)
-
-    jal ra dot
-    mv s5, a0
-
-    blt t1, a5, inner_loop_end
 
 inner_loop_end:
-    mul t3, s1, t0
-    add t3, t1, t3
-    slli t3, t3, 2
-    add t3, t3, s3
-
-    lw a6, 0(t3)
-
-    j outer_loop_start
-
 
 outer_loop_end:
-    mv a0, s5
     # Epilogue
     
     
